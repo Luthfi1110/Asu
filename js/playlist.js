fc49ad03bf8d22becd3f856849e17223d2682c87
Luthfi1111 <124867339+Luthfi1111@users.noreply.github.com>
2023-02-09 14:34:12 +0700

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "D'MASIV - Diam Tanpa Kata",
+    artist: "D'Masiv",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "DJ Alone x Sampe Bawah",
+    artist: "Unknown",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Lil Ngabers - Hayang Aerox",
+    artist: "Lil Ngabers",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Desert Planet - Quincas",
+    artist: "Quincas",
+    img: "img4",
+    audio: "music4"
+  }
+]
